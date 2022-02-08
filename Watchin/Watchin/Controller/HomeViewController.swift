6cbf8414710abfc3c55ce26b81df47e530de382a
Laura Jacquin <laura.jcqn@live.fr>
2022-02-08 16:45:13 +0100

    started working on home scene interface, added profile imageView, edit button, a label and outlets to corresponding controller

--- Watchin/Watchin/Controller/HomeViewController.swift
+++ Watchin/Watchin/Controller/HomeViewController.swift
@@ -9,21 +9,34 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var editProfileButton: UIButton!
+    @IBOutlet weak var userNameLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+        setImageAspect()
+        setEditButtonAspect()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+
+    // MARK: - Private
+
+    private func setImageAspect() {
+        profileImageView.layer.borderWidth = 3
+        profileImageView.layer.masksToBounds = false
+        profileImageView.layer.borderColor = UIColor.white.cgColor
+        profileImageView.layer.cornerRadius = profileImageView.frame.height/2
+        profileImageView.clipsToBounds = true
+    }
+
+    private func setEditButtonAspect() {
+        editProfileButton.layer.borderWidth = 1
+        editProfileButton.layer.borderColor = UIColor.white.cgColor
+        editProfileButton.layer.cornerRadius = 10
+    }
 
 }
