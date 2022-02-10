ca74b14de07ec42f0504eea2d2ccca2dd1ea175e
Laura Jacquin <laura.jcqn@live.fr>
2022-02-10 18:29:02 +0100

    added getSearchResults() in TvShowService

--- Watchin/Watchin/Model/TvShowService.swift
+++ Watchin/Watchin/Model/TvShowService.swift
@@ -11,7 +11,30 @@ class TvShowService {
 
     // MARK: - Properties
 
+    private let networkService: NetworkProtocol
     private let baseUrl = "https://www.episodate.com/api/"
+
+    // MARK: - Init
+
+    init(networkService: NetworkProtocol = NetworkService()) {
+        self.networkService = networkService
+    }
+
+    // MARK: - getSearchResults
+
+    func getSearchResults(searchText: String?, completion: @escaping (Result<[TvShowsSearchDetail], Error>) -> Void) {
+        let baseUrl = computeShowDetailsQuery()
+
+        networkService.request(baseURL: baseUrl) { (result: Result<SearchResult, Error>) in
+            switch result {
+            case .success(let searchResult):
+                let tvShows = searchResult.tv_shows
+                completion(.success(tvShows))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
 
     // MARK: - Private
 
