809720869ccd7b0902bdd5b9f5e6813763a96265
Laura Jacquin <laura.jcqn@live.fr>
2022-02-10 16:57:39 +0100

    added Platforms file with platformNames

---
+++ Watchin/Watchin/Model/Platforms.swift
@@ -0,0 +1,32 @@
+//
+//  Platforms.swift
+//  Watchin
+//
+//  Created by Archeron on 10/02/2022.
+//
+
+import Foundation
+
+let platformNames = ["Netflix",
+                     "Prime Video",
+                     "Disney +",
+                     "HBO Max",
+                     "Hulu",
+                     "OCS",
+                     "Salto",
+                     "StarzTV",
+                     "AppleTV",
+                     "RakutenTV",
+                     "MyCanal",
+                     "SFR Play",
+                     "Peacock",
+                     "Paramount +",
+                     "ADN",
+                     "Crunchyroll",
+                     "Discovery +",
+                     "Crackle",
+                     "AcornTV",
+                     "PlutoTV",
+                     "Shadowz",
+                     "Funimation"]
+
