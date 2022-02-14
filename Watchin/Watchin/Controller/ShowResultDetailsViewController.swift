8431376c6835b7087d0723aea40390ad9a546f61
Laura Jacquin <laura.jcqn@live.fr>
2022-02-14 16:17:09 +0100

    cleaned searchResultDetails interface, changed color of Back button on navigation bar

--- Watchin/Watchin/Controller/ShowResultDetailsViewController.swift
+++ Watchin/Watchin/Controller/ShowResultDetailsViewController.swift
@@ -15,12 +15,26 @@ class ShowResultDetailsViewController: UIViewController {
 
     // MARK: - Outlets
     
+    @IBOutlet weak var tvShowPosterImageView: UIImageView!
+    @IBOutlet weak var showTitleLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var genresLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var seasonCountLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var addToYourShowsButton: UIButton!
+    @IBOutlet weak var addToWatchinLaterButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var goToWikipediaButton: UIButton!
 
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setButtonAspect(for: addToYourShowsButton)
+        setButtonAspect(for: addToWatchinLaterButton)
+        setButtonAspect(for: goToWikipediaButton)
     }
 
     // MARK: - Actions
---
+++ Watchin/Watchin/View/UIColor+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor+Extension.swift
+//  Watchin
+//
+//  Created by Archeron on 14/02/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    convenience init(red: Int, green: Int, blue: Int, a: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: a
+        )
+    }
+}
