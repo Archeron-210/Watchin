11a9d93aefd029c40712732e5809df02be89d45f
Laura Jacquin <laura.jcqn@live.fr>
2022-02-15 15:22:38 +0100

    added CoreData to project, with CoreDataStack and a protocol wich will be used in unit tests

---
+++ Watchin/Watchin/Model/CoreData/CoreDataStack.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataStack.swift
+//  Watchin
+//
+//  Created by Archeron on 15/02/2022.
+//
+
+import Foundation
+import CoreData
+
+protocol CoreDataStackProtocol {
+    var viewContext: NSManagedObjectContext { get }
+}
+
+class CoreDataStack: CoreDataStackProtocol {
+
+    // MARK: - Singleton
+
+    static let shared = CoreDataStack()
+
+    // MARK: - Public
+
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+
+    // MARK: - Private
+
+    private init() {}
+
+    private static let modelName = "Watchin"
+
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: CoreDataStack.modelName)
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo) for : \(storeDescription.description)")
+            }
+        }
+        return container
+    }()
+}
