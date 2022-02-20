31dab1d4ab21566371406d2c91fe83f9aeba3ca7
Laura Jacquin <laura.jcqn@live.fr>
2022-02-20 18:26:51 +0100

    started working on CoreData entities : 2 created, WatchinShow and Episode, with corresponding attributes and relationship defined

---
+++ Watchin/Watchin/Model/Episode.swift
@@ -0,0 +1,11 @@
+//
+//  Episode.swift
+//  Watchin
+//
+//  Created by Archeron on 20/02/2022.
+//
+
+import Foundation
+import CoreData
+
+class Episode: NSManagedObject {}
---
+++ Watchin/Watchin/Model/WatchinShow.swift
@@ -0,0 +1,11 @@
+//
+//  WatchinShow.swift
+//  Watchin
+//
+//  Created by Archeron on 20/02/2022.
+//
+
+import Foundation
+import CoreData
+
+class WatchinShow: NSManagedObject {}
