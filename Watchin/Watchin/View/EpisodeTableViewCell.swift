f305ad9135dfb0e11ab0d9d9a84aec55fea6e8cd
Laura Jacquin <laura.jcqn@live.fr>
2022-02-28 17:29:02 +0100

    Configured UI aspect of button 'Saw it' in episodeCell

--- Watchin/Watchin/View/EpisodeTableViewCell.swift
+++ Watchin/Watchin/View/EpisodeTableViewCell.swift
@@ -15,6 +15,13 @@ class EpisodeTableViewCell: UITableViewCell {
     @IBOutlet weak var episodeTitleLabel: UILabel!
     @IBOutlet weak var sawItButton: UIButton!
 
+    // MARK: - Life Cycle
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setButtonAspect()
+    }
+
     // MARK: - Configure
 
     func configure(for episode: EpisodeFormatted) {
@@ -23,6 +30,12 @@ class EpisodeTableViewCell: UITableViewCell {
         episodeTitleLabel.text = episode.episodeNameFormatted
     }
 
-    
+    // MARK: - Private
+
+    private func setButtonAspect() {
+        sawItButton.layer.borderWidth = 1
+        sawItButton.layer.borderColor = UIColor.white.cgColor
+        sawItButton.layer.cornerRadius = 10
+    }
 
 }
